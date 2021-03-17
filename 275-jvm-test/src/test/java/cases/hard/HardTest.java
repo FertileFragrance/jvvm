de2c211724d877357e3ea741a122f97144c86632
FertileFragrance <1016030318@qq.com>
2021-03-18 00:20:21 +0800

    to remote

---
+++ 275-jvm-test/src/test/java/cases/hard/HardTest.java
@@ -0,0 +1,12 @@
+package cases.hard;
+
+public class HardTest {
+
+    char[] letters = new char[26];
+
+    public static void main(String[] args) {
+        HardTest temp = new HardTest();
+        temp.letters[0] = 'a';
+    }
+
+}
